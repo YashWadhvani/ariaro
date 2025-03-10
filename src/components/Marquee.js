092b2c981b7ebb5a8c315365b2e52df1e1c76287
YashWadhvani <yashwadhvani123@gmail.com>
2025-03-10 08:57:02 +0530

    Added all the Logos and Marquee for Eco System Partners

---
+++ src/components/Marquee.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../styles/Marquee.css";
+import MeitY from "../assets/Logos/Meity.png";
+import SSIP from "../assets/Logos/SSIP.png";
+import KIIF from "../assets/Logos/KIFF.png";
+import AWS from "../assets/Logos/AWS.png";
+import RedHat from "../assets/Logos/redhat.png";
+import ElectroMech from "../assets/Logos/electromech.png";
+import IIC from "../assets/Logos/iiclogo.png";
+import SeedFund from "../assets/Logos/seedfund.png";
+import GDG from "../assets/Logos/GDG-KU.png";
+import EduNet from "../assets/Logos/edunet.png";
+
+const partners = [
+    MeitY,
+    SSIP,
+    KIIF,
+    AWS,
+    RedHat,
+    ElectroMech,
+    IIC,
+    SeedFund,
+    GDG,
+    EduNet,
+]; // Add more as needed
+
+function Marquee() {
+    return (
+        <>
+        <h2 style={{textAlign:"center", margin:"1rem 0"}}>Eco System Partners :</h2>
+        <div className="marquee-container">
+            <div className="marquee">
+                <div className="marquee-track">
+                    {partners.concat(partners).map((logo, index) => (
+                        <img
+                            key={index}
+                            src={logo}
+                            alt={`Partner ${index + 1}`}
+                            className="marquee-logo"
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Marquee;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import Marquee from "../components/Marquee";
 
 function Home() {
     return (
         <>
             <HeroSection />
+            <Marquee/>
         </>
     );
 }
